eda9610bfca7a37bb3ca402363ad4f3f65183323
ganeshpinnelli <ganeshpinnelli2306@gmail.com>
2026-08-27 22:27:40 +0530

    Time: 1 ms (98.54%) | Memory: 42.7 MB (23.58%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) {
+            return x;
+        }
+
+        int left = 1;
+        int right = x / 2;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Avoid overflow: instead of mid * mid <= x
+            if (mid <= x / mid) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return right;
+    }
+}
